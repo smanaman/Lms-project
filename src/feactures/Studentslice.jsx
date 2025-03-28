6a806af5bb717d5bfb2b656538e466ec3ad0b8f6
smanaman <sanvatsman@gmail.com>
2025-03-28 10:42:39 +0530

    studentdata

--- src/feactures/Studentslice.jsx
+++ src/feactures/Studentslice.jsx
@@ -1,24 +1,30 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialState={
-    admin:JSON.parse(localStorage.getItem('AdminRegister')) || []
+const initialState = {
+    admin: JSON.parse(localStorage.getItem('AdminRegister')) || []
 
 }
-const Studentslice=createSlice({
-name:'admin',
-initialState,
-reducers:{
-    addadmin:(state,action)=>{
-state.admin.push({...action.payload,id:nanoid()})
-    },
-    loout:(state,action)=>{
-localStorage.removeItem('AdminRegister')
-state.admin=[]
+const Studentslice = createSlice({
+    name: 'admin',
+    initialState,
+    reducers: {
+        addadmin: (state, action) => {
+            state.admin.push({ ...action.payload, id: nanoid() })
+        },
+        loout: (state, action) => {
+            localStorage.removeItem('AdminRegister')
+            state.admin = []
+        },
+        AddStudent: (state, action) => {
+        const student=JSON.parse(localStorage.getItem('setstudent')) ||[]
+        const newstudent={...action.payload,id:nanoid()}
+        student.push(newstudent)
+        localStorage.setItem('StudentData',JSON.stringify(student))
+        }
     }
-}
 
 })
 
 
 export default Studentslice.reducer
-export const {addadmin,loout} =Studentslice.actions
+export const { addadmin, loout,AddStudent} = Studentslice.actions
